7c36571e5663cd2a9dc6e18731977c3695546b95
thiago beserra <thiago.beserra@luizalabs.com>
2018-12-27 14:08:27 -0200

    iniciou estrutura do projeto

---
+++ src/main/java/br/com/sapri/workercarga/WorkerCargaApplication.java
@@ -0,0 +1,14 @@
+package br.com.sapri.workercarga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkerCargaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkerCargaApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/br/com/sapri/workercarga/WorkerCargaApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.sapri.workercarga;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WorkerCargaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
